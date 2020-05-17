b2d2a32e1f97ccfd4d931fbb6f80d35b2c179768
Maksym Bryzhko <maxim.bryzhko@gmail.com>
2020-05-17 16:54:26 +0300

    Get Album by name

--- src/main/java/bma/photo/uploader/commands/AlbumCommand.java
+++ src/main/java/bma/photo/uploader/commands/AlbumCommand.java
@@ -6,6 +6,7 @@ import bma.photo.uploader.google.GooglePhotoApiClientFactory;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.google.api.gax.rpc.ApiException;
+import com.google.api.gax.rpc.InvalidArgumentException;
 import com.google.photos.library.v1.PhotosLibraryClient;
 import com.google.photos.types.proto.Album;
 import lombok.Getter;
@@ -16,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import static bma.photo.uploader.config.ApplicationProperties.GOOGLE_PHOTO_RROPS_NAME;
 
@@ -26,9 +28,14 @@ public class AlbumCommand {
 
     private static final Logger logger = LoggerFactory.getLogger(AlbumCommand.class);
 
-    @Parameter(names = "-id", description = "AlbumId", required = true)
+    @Parameter(names = "-id", description = "AlbumId", required = false)
     @Setter @Getter
     private String albumId;
+
+    @Parameter(names = "-name", description = "AlbumName", required = false)
+    @Setter @Getter
+    private String albumName;
+
 
     private final GooglePhotoApiClientFactory apiClientFactory;
     private final ApplicationProperties applicationProperties;
@@ -40,20 +47,47 @@ public class AlbumCommand {
     }
 
     public void run() {
-
         boolean hasGoogleProps = applicationProperties.getUploaders().containsKey(GOOGLE_PHOTO_RROPS_NAME);
+
+        if (albumId == null && albumName == null) {
+            logger.error("Album 'id' or 'name' should be specified");
+            return;
+        }
 
         if (hasGoogleProps) {
             UploaderProperties googlePhotoProps = applicationProperties.getUploaders().get(GOOGLE_PHOTO_RROPS_NAME);
             try (PhotosLibraryClient client = apiClientFactory.create(googlePhotoProps::getCredentials)) {
-                Album album = client.getAlbum(albumId);
-
-                logger.info("Album General Info: {}", album.toString());
-                logger.info("Album Sharing Info: {}", album.getShareInfo().getSharedAlbumOptions());
-
+                findAlbum(client).ifPresentOrElse(this::printAlbumDetails, () -> logger.error("Album not found"));
             } catch (IOException | ApiException e) {
-                logger.error("Cannot get album info, id: {}, error: {}", albumId, e.getMessage());
+                logger.error("Cannot print album info, error: {}", e.getMessage());
             }
         }
+    }
+
+    private void printAlbumDetails(Album album) {
+        logger.info("Album General Info:");
+        logger.info("  Id = {}", album.getId());
+        logger.info("  Title = {}", album.getTitle());
+        logger.info("  URL = {}", album.getProductUrl());
+        logger.info("Album Sharing Info: {}", album.getShareInfo().getSharedAlbumOptions());
+    }
+
+    private Optional<Album> findAlbum(PhotosLibraryClient client)  {
+        if (albumId != null) {
+            try {
+                logger.info("Search album by id: {}", albumId);
+                return Optional.of(client.getAlbum(albumId));
+            } catch (InvalidArgumentException e) {
+                return Optional.empty();
+            }
+        } else if (albumName != null) {
+            logger.info("Search album by name: {}", albumName);
+            for (Album album : client.listSharedAlbums().iterateAll()) {
+                if (album.getTitle().equalsIgnoreCase(albumName)) {
+                    return Optional.of(album);
+                }
+            }
+        }
+        return Optional.empty();
     }
 }
--- src/test/java/bma/photo/uploader/commands/AlbumCommandTest.java
+++ src/test/java/bma/photo/uploader/commands/AlbumCommandTest.java
@@ -5,7 +5,9 @@ import bma.photo.uploader.config.UploaderProperties;
 import bma.photo.uploader.google.GooglePhotoApiClientFactory;
 import bma.photo.uploader.uploader.CredentialsAware;
 import com.google.photos.library.v1.PhotosLibraryClient;
+import com.google.photos.library.v1.internal.InternalPhotosLibraryClient;
 import com.google.photos.types.proto.Album;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,6 +26,8 @@ import static bma.photo.uploader.config.ApplicationProperties.GOOGLE_PHOTO_RROPS
 class AlbumCommandTest {
 
     public static final String ALBUM_ID = "abc";
+    public static final String ALBUM_NAME = "name";
+
     private AlbumCommand command;
     private ApplicationProperties properties;
     
@@ -32,27 +36,66 @@ class AlbumCommandTest {
 
     @Mock
     private PhotosLibraryClient client;
+
+    @Mock
+    private InternalPhotosLibraryClient.ListSharedAlbumsPagedResponse albums;
     
     @BeforeEach
     public void setup() {
         properties = new ApplicationProperties();
         command = new AlbumCommand(apiClientFactory, properties);    
     }
-    
+
+    @Test
+    public void validationErrorWhenNeitherIdNorNameSpecified() throws IOException {
+
+        command.run();
+
+        Mockito.verify(apiClientFactory, Mockito.times(0)).create(Mockito.isA(CredentialsAware.class));
+    }
+
     @Test
     public void albumInfoIsRetrievedById() throws IOException {
         command.setAlbumId(ALBUM_ID);
+        var testAlbum = Mockito.spy(newTestAlbum(ALBUM_ID, ALBUM_NAME));
 
         var credentials = propertiesWithCredentials();
 
         Mockito.when(apiClientFactory.create(Mockito.isA(CredentialsAware.class))).thenReturn(client);
-        Mockito.when(client.getAlbum("abc")).thenReturn(Album.newBuilder().setId(ALBUM_ID).build());
+        Mockito.when(client.getAlbum(ALBUM_ID)).thenReturn(testAlbum);
 
         command.run();
 
         ArgumentCaptor<CredentialsAware> credentialsCaptor = ArgumentCaptor.forClass(CredentialsAware.class);
         Mockito.verify(apiClientFactory, Mockito.only()).create(credentialsCaptor.capture());
         Assertions.assertEquals(credentials, credentialsCaptor.getValue().getCredentials());
+
+        Mockito.verify(testAlbum).getId();
+        Mockito.verify(testAlbum).getTitle();
+        Mockito.verify(testAlbum).getProductUrl();
+        Mockito.verify(testAlbum).getShareInfo();
+    }
+
+    @Test
+    public void albumInfoIsRetrievedByName() throws IOException {
+        command.setAlbumName(ALBUM_NAME);
+        var testAlbum = Mockito.spy(newTestAlbum(ALBUM_ID, ALBUM_NAME));
+
+        var credentials = propertiesWithCredentials();
+        Mockito.when(apiClientFactory.create(Mockito.isA(CredentialsAware.class))).thenReturn(client);
+        Mockito.when(client.listSharedAlbums()).thenReturn(albums);
+        Mockito.when(albums.iterateAll()).thenReturn(Lists.list(newTestAlbum("foo", "bar"), testAlbum));
+
+        command.run();
+
+        Mockito.verify(testAlbum).getId();
+        Mockito.verify(testAlbum, Mockito.times(2)).getTitle();
+        Mockito.verify(testAlbum).getProductUrl();
+        Mockito.verify(testAlbum).getShareInfo();
+    }
+
+    private Album newTestAlbum(String id, String name) {
+        return Album.newBuilder().setId(id).setTitle(name).build();
     }
 
     private Map<String, String> propertiesWithCredentials() {
@@ -65,6 +108,5 @@ class AlbumCommandTest {
 
         return credentials;
     }
-
 
 }
